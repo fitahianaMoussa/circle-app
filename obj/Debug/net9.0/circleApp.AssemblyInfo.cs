3e38fd3e2c694e33bd77a79381f99b1054166d11
fitahianaMoussa <fitahianamoussa3@gmail.com>
2025-03-04 22:10:34 +0400

    implement and configure asset

--- obj/Debug/net9.0/circleApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/circleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("circleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2ebf107bb1d987ff59595714d6477fac802d262")]
 [assembly: System.Reflection.AssemblyProductAttribute("circleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("circleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
